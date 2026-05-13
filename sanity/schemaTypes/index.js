bb6675d09c57836ada13e421298b2bcdc58b3106
Salome Egwumba <salome.egwumba@leveneenergy.com>
2026-05-13 16:54:41 +0100

    update

---
+++ sanity/schemaTypes/index.js
@@ -0,0 +1,9 @@
+import post from './post'
+import subsidiary from './subsidiary'
+import mediaItem from './mediaItem'
+import mediaFolder from './mediaFolder'
+import pressRelease from './pressRelease'
+import teamMember from './teamMember'
+
+
+export const schemaTypes = [post, subsidiary, mediaItem, mediaFolder, pressRelease, teamMember]
